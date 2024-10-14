fbc3be4a9eb4bb96bc91047d29daa95c3d5dac50
muneeb_063 <muneeb.asif063@gmail.com>
2024-10-14 15:54:52 +0500

    FPS setup and raycast handled

---
+++ Assets/ItemDetector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDetector : MonoBehaviour
+{
+    [SerializeField] float range = 100;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] bool isOnItem = false;
+    [SerializeField] Image pointer;
+    public bool InOnItem { get=>isOnItem;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isOnItem = Physics.Raycast(transform.position, transform.forward, range, layerMask);
+
+        pointer.color = Color.white;
+        if (isOnItem) pointer.color = Color.green;
+        
+    }
+}
